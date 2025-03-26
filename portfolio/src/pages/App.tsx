45bb6097034ecb78069d41bb3d9c3d31554d5028
Ranjot Dharni <ranjotdharni1@gmail.com>
2025-03-26 12:40:05 -0700

    project dependencies instated

---
+++ portfolio/src/pages/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <main className="w-full h-screen bg-slate-500 flex flex-col lg:flex-row items-center justify-evenly">
+      <h1 className="text-3xl text-teal-800">Hello, World!</h1>
+      <h2 className="text-3xl text-teal-600">This is another text.</h2>
+      <h3 className="text-3xl text-teal-400">And yet another one?!</h3>
+    </main>
+  )
+}
+
+export default App
