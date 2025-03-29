5a154b843d95a66f815de58db2af8f53461b1fbf
Ranjot Dharni <ranjotdharni1@gmail.com>
2025-03-28 23:16:32 -0700

    added spline graphics

--- portfolio/src/components/templates/Landing.tsx
+++ portfolio/src/components/templates/Landing.tsx
@@ -5,7 +5,7 @@ import LandingIntro from "../organisms/LandingIntro"
 
 export default function Landing() {
     let height = 100 - navHeightNumeric
-    let css = `w-full z-10 px-4 relative top-[${navHeight}]`
+    let css = `w-full z-10 overflow-visible relative top-[${navHeight}]`
 
     return (
         <section className={css} style={{height: `${height}vh`}}>
