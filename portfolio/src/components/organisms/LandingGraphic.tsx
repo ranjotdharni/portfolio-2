f210239938eaece27de2af57e775eb4db8d3c0c9
Robby Dharni <robbydharni@Robbys-MacBook-Pro.local>
2025-03-28 23:16:32 -0700

    added spline graphics

--- portfolio/src/components/organisms/LandingGraphic.tsx
+++ portfolio/src/components/organisms/LandingGraphic.tsx
@@ -1,10 +1,10 @@
-import LandingGraphicImage from '../../assets/images/landingImage.png'
+
 
 export default function LandingGraphic() {
-
+    
     return (
-        <div className="w-full h-1/2 p-4 flex flex-col justify-center items-center">
-            <img className='w-full h-full object-cover' src={LandingGraphicImage} />
+        <div className="w-full h-1/2 p-4 flex flex-col justify-center items-center overflow-visible">
+            <iframe src='https://my.spline.design/roomrelaxingcopy-d771332ca2c1b974dd9708b657ea7f99/' width='100%' height='100%'></iframe>
         </div>
     )
 }
