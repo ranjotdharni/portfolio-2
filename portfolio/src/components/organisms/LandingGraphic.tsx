b151b91fe9babb3b63a68dfb6dacc4442696a86f
Robby Dharni <robbydharni@Robbys-MacBook-Pro.local>
2025-03-29 20:46:39 -0700

    responsive landing

--- portfolio/src/components/organisms/LandingGraphic.tsx
+++ portfolio/src/components/organisms/LandingGraphic.tsx
@@ -3,8 +3,8 @@
 export default function LandingGraphic() {
     
     return (
-        <div className="w-full h-1/2 p-4 flex flex-col justify-center items-center overflow-visible">
-            <iframe src='https://my.spline.design/roomrelaxingcopy-d771332ca2c1b974dd9708b657ea7f99/' width='100%' height='100%'></iframe>
+        <div className="w-full h-1/2 p-4 flex flex-col justify-center items-center overflow-visible md:w-1/2 md:h-full">
+            <iframe src='https://my.spline.design/roomrelaxingcopy-d771332ca2c1b974dd9708b657ea7f99/' className="w-full h-full"></iframe>
         </div>
     )
 }
--- portfolio/src/components/templates/Landing.tsx
+++ portfolio/src/components/templates/Landing.tsx
@@ -1,11 +1,11 @@
-import LandingGraphic from "../organisms/LandingGraphic"
-import { navHeight, navHeightNumeric } from "../../lib/constants/layout"
-import LandingIntro from "../organisms/LandingIntro"
+import { navHeight, navHeightNumeric } from '../../lib/constants/layout'
+import LandingGraphic from '../organisms/LandingGraphic'
+import LandingIntro from '../organisms/LandingIntro'
 
 
 export default function Landing() {
     let height = 100 - navHeightNumeric
-    let css = `w-full z-10 overflow-visible relative top-[${navHeight}]`
+    let css = `w-full z-10 overflow-visible relative top-[${navHeight}] md:flex md:flex-row`
 
     return (
         <section className={css} style={{height: `${height}vh`}}>
