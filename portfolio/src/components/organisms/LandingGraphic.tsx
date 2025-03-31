706171bbf2c665c7abf5792a36b2a175caf978fc
Robby Dharni <robbydharni@Robbys-MacBook-Pro.local>
2025-03-30 22:48:43 -0700

    experience section complete (responsive)

--- portfolio/src/components/organisms/LandingGraphic.tsx
+++ portfolio/src/components/organisms/LandingGraphic.tsx
@@ -4,10 +4,8 @@ import '../../styles/animation.css'
 export default function LandingGraphic() {
     
     return (
-        <div className="w-full h-1/2 p-4 flex flex-col justify-center items-center overflow-visible md:w-1/2 md:h-full">
+        <div className="w-full h-1/2 p-4 relative -top-[10vh] flex flex-col justify-center items-center overflow-visible md:w-1/2 md:h-full md:top-0">
             <img src={LandingGraphicImage} className='h-full w-full object-cover slowBounce' />
         </div>
     )
 }
-
-// <iframe src='https://my.spline.design/roomrelaxingcopy-d771332ca2c1b974dd9708b657ea7f99/' className="w-full h-full"></iframe>
--- portfolio/src/components/templates/Landing.tsx
+++ portfolio/src/components/templates/Landing.tsx
@@ -1,14 +1,13 @@
-import { navHeight, navHeightNumeric } from '../../lib/constants/layout'
+import { navHeight } from '../../lib/constants/layout'
 import LandingGraphic from '../organisms/LandingGraphic'
 import LandingIntro from '../organisms/LandingIntro'
 
 
 export default function Landing() {
-    let height = 100 - navHeightNumeric
-    let css = `w-full z-10 overflow-visible relative top-[${navHeight}] md:flex md:flex-row`
+    let css = `w-full h-[100vh] z-10 overflow-visible relative top-[${navHeight}] md:top-0 md:flex md:flex-row`
 
     return (
-        <section className={css} style={{height: `${height}vh`}}>
+        <section className={css}>
             <LandingIntro />
             <LandingGraphic />
         </section>
