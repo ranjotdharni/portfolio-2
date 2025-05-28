54f0cd74562508568dab7d16c846007f87660742
Ranjot Dharni <ranjotdharni1@gmail.com>
2025-05-28 12:08:21 -0700

    add chat application to projects

--- portfolio/src/components/organisms/Carousel.tsx
+++ portfolio/src/components/organisms/Carousel.tsx
@@ -5,45 +5,52 @@ import '../../styles/animation.css'
 
 const projectData: CarouselCardParams[] = [
     {
+        title: 'ChemiStreams',
+        subtitle: 'Realtime Chat Application',
+        link: 'https://chemistreams.vercel.app/login',
+        siteImgIndex: 0, // See CarouselCard.tsx for reference
+        desc: 'A browser-to-browser messaging service for chatting with your friends in real time. Message your friends, create group chats, and customize your profile to stand out. Share your favorite music and videos with your friends using our Spotify and YouTube embedding features.'
+    },
+    {
         title: 'Hegemony',
         subtitle: 'Virtual Economy Game',
         link: 'https://hegemony.vercel.app/',
-        siteImgIndex: 0, // See CarouselCard.tsx for reference
+        siteImgIndex: 1,
         desc: 'An idle game that lets you interact with and contribute to a virtual economy in a fantasy-like setting. Invite other players and make friends to build up your wealth. Select an Empire, create Cities, and even start your own States in this browser-based web app!'
     },
     {
         title: 'Voxyl',
         subtitle: 'Project Management Service',
         link: 'https://pitcrew.onrender.com/',
-        siteImgIndex: 1,
+        siteImgIndex: 2,
         desc: 'Have a team? Have an idea? Now all you need is a platform to bring the two together. Voxyl is your one stop shop for organizing teams, creating projects, assigning roles, and monitoring performance. Sign up to create your first Team, recruit Members, and assign permissions.'
     },
     {
         title: 'Byte My Courses',
         subtitle: 'Schoolwork Assistant AI',
         link: 'https://github.com/timothydn2003/ByteMe',
-        siteImgIndex: 2,
+        siteImgIndex: 3,
         desc: "Being in school can be tough, especially when pursuing higher education. You have so many responsibilities that sometimes it's hard to keep up. Luckily, this AI service that can take your notes, books, and other files and turn them into short, digestible reports."
     },
     {
         title: 'Sac 311 Mobile',
         subtitle: 'Customer Service Mobile App',
         link: 'https://github.com/ranjotdharni/sac-311-mobile',
-        siteImgIndex: 3,
+        siteImgIndex: 4,
         desc: "City of Sacramento's 311 mobile service developed on React Native using the Expo framework. Make non-emergency customer service requests as a city resident, visitor, or business all at the click of a button from convenience of your mobile device."
     },
     {
         title: 'Tile Map Maker',
         subtitle: 'Image Generation Service',
         link: 'http://159.54.169.148:443/',
-        siteImgIndex: 4,
+        siteImgIndex: 5,
         desc: "A powerful procedural generation application designed to create 1920x1080 images of a 2D tile map from a set of basic tile images. Whether you're a developer or a user interested in generating unique visual content, this application provides a seamless experience."
     },
     {
         title: 'Meteorize',
         subtitle: 'Real-time Weather Service',
         link: 'https://meteorize.netlify.app',
-        siteImgIndex: 5,
+        siteImgIndex: 6,
         desc: "A simple tool for viewing the current, daily, and weekly forecasts around the world. Search for a location and add them to your weather queue to quickly find their forecast again and again at the click of a button, and all with an easy-to-use user interface."
     }
 ]
