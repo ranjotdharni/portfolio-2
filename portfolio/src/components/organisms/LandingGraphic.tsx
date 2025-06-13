d39f0fb1c7d2cb61f40bd3d93e725c5d60ea59ed
Ranjot Dharni <ranjotdharni1@gmail.com>
2025-06-13 06:10:00 -0700

    3d model update

--- portfolio/src/components/organisms/LandingGraphic.tsx
+++ portfolio/src/components/organisms/LandingGraphic.tsx
@@ -1,11 +1,10 @@
-import LandingGraphicImage from '../../assets/images/landingImage.png'
 import '../../styles/animation.css'
 
 export default function LandingGraphic() {
     
     return (
-        <div className="w-full h-1/2 p-4 relative -top-[10vh] flex flex-col justify-center items-center overflow-visible md:w-1/2 md:h-full md:top-0">
-            <img src={LandingGraphicImage} className='h-full w-full object-cover slowBounce' />
+        <div className="w-full h-1/2 p-4 relative -top-[10vh] flex flex-col justify-center items-center overflow-visible md:w-2/3 md:h-full md:top-0">
+            <iframe src='https://my.spline.design/roomrelaxingcopy-d771332ca2c1b974dd9708b657ea7f99/' width='100%' height='100%'></iframe>
         </div>
     )
 }
--- portfolio/src/components/organisms/LandingIntro.tsx
+++ portfolio/src/components/organisms/LandingIntro.tsx
@@ -100,8 +100,8 @@ export default function LandingIntro(params: { quickContact: () => void }) {
     }, [])
 
     return (
-        <div className="w-full h-1/2 p-4 flex flex-col justify-start items-center space-y-4 md:w-1/2 md:h-full md:justify-center">
-            <p className="text-color-white font-jbm text-xl w-full text-left md:w-1/2 md:text-3xl">HELLO, MY NAME IS</p>
+        <div className="w-full h-1/2 p-4 flex flex-col justify-start items-center space-y-4 md:w-1/3 md:h-full md:justify-center">
+            <p className="text-color-white font-jbm text-xl w-full text-left md:w-1/2 md:text-2xl">HELLO, MY NAME IS</p>
             <div className="w-full flex flex-row justify-start items-center space-x-4 md:w-1/2">
                 <div className="flex flex-row justify-start items-center">
                     <AnimatedText text={name.firstName} animationClass={animation} />
@@ -110,7 +110,7 @@ export default function LandingIntro(params: { quickContact: () => void }) {
                     <AnimatedText text={name.lastName} animationClass={animation} />
                 </div>
             </div>
-            <p className="text-color-light-grey-text w-full text-left font-jbm md:w-1/2 md:text-xl">
+            <p className="text-color-light-grey-text w-full text-left font-jbm md:relative md:left-[8.335%] md:w-2/3 md:text-lg">
                 I'm a full-stack web developer holding a Bachelor's degree in Computer Science. I'm currently looking for a position related to IT or development. Use the links above to navigate the site or keep scrolling if you're on mobile to see more of my work!
             </p>
             <div className="w-full h-10 md:w-1/2">
